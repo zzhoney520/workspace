e615d3ad37331af1f0bcfc64847628361c6337f1
ZZHONEY <zzhoney520@outlook.com>
2021-06-06 13:41:25 +0900

    update

--- demo/src/main/java/com/example/demo/controller/MyController.java
+++ demo/src/main/java/com/example/demo/controller/MyController.java
@@ -1,14 +1,19 @@
 package com.example.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.Data;
+import lombok.extern.log4j.Log4j2;
 
 @RestController
 @ConfigurationProperties(prefix = "com.honey.home")
 @Data
+@Log4j2
 public class MyController {
 	public String id;
 	public String firstName;
@@ -19,18 +24,19 @@ public class MyController {
 	public int age;
 
 	@GetMapping("/getid")
-	public String showID() {
-		return this.getId();
+	public String showID(@Value("${random.int.5,100;}") String strID) {
+		return strID;
 	}
 
 	@GetMapping("/getfirstname")
-	public String showFirstName() {
-		return String.format("%s", this.getFirstName());
+	public String showFirstName(@Value("${com.honey.home.firstname}") String firstName) {
+		return String.format("%s", firstName);
 	}
 
 	@GetMapping("/getage")
 	public int showAge() {
 		return this.getAge();
 	}
-
+		
+	
 }
